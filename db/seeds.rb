a88515c7666e74e6acc3cc31a9133c720abd7a3b
nikibor <nborgolov96@gmail.com>
2018-04-02 20:16:16 +0400

    Add seeds for deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+role_test = Role.create(title: 'ROLE_TEST')
+role_admin = Role.create(title: 'ADMIN')
+role_guest = Role.create(title: 'GUEST')
+
+status_active = Status.create(state: 'Active')
+status_disabled = Status.create(state: 'Disabled')
+
+user1 = User.create(role: role_test, status: status_active, email: 'test@test.com', name: 'Vasiliy',
+                    surname: 'Vasilyev', birth: DateTime.new(2018,4,4))
+user2 = User.create(role: role_admin, status: status_active, email: 'test2@test.com', name: 'Vasiliy',
+                    surname: 'Vasilyev', birth: DateTime.new(2018,4,5))
+user3 = User.create(role: role_guest, status: status_disabled, email: 'test3@test.com', name: 'Vasiliy',
+                    surname: 'Vasilyev', birth: DateTime.new(2018,4,6))
