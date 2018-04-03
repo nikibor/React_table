538645f75f6f1ffbd09697551149b5ac578633a8
nikibor <nborgolov96@gmail.com>
2018-04-03 23:01:52 +0400

    Refactoring

--- app/assets/javascripts/components/user.js.jsx
+++ app/assets/javascripts/components/user.js.jsx
@@ -1,17 +1,17 @@
 var User = createReactClass({
 
   render: function() {
+      let user = this.props.user;
     return (
       <React.Fragment>
           <tr>
-              <td>
-                  <h6>{this.props.user.id}</h6>
-                  {/*asdasd*/}
-              </td>
-              <td>
-                  <h6>{this.props.user.name}</h6>
-                  {/*qweqweqwe*/}
-              </td>
+              <td><h6>{user.id}</h6></td>
+              <td><h6>{user.role_id}</h6></td>
+              <td><h6>{user.status_id}</h6></td>
+              <td><h6>{user.email}</h6></td>
+              <td><h6>{user.name}</h6></td>
+              <td><h6>{user.surname}</h6></td>
+              <td><h6>{user.birth}</h6></td>
           </tr>
       </React.Fragment>
     );
--- app/assets/javascripts/components/users.js.jsx
+++ app/assets/javascripts/components/users.js.jsx
@@ -1,20 +1,27 @@
 var Users = createReactClass({
   render: function() {
-      users = JSON.parse(this.props.data);
-    return (
-      <React.Fragment>
+      let users = JSON.parse(this.props.data);
+      return <React.Fragment>
+          <h3>Управление пользователями</h3>
           <table className={'table'}>
               <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
+              <tr>
+                  <th>#</th>
+                  <th>Роли</th>
+                  <th>Статус</th>
+                  <th>E-mail</th>
+                  <th>Имя</th>
+                  <th>Фамилия</th>
+                  <th>Дата рождения</th>
+              </tr>
               </thead>
               <tbody>
-              <User user={users[2]}></User>
+                  <User user={users[0]}/>
+                  <User user={users[1]}/>
+                  <User user={users[2]}/>
+                  <User user={users[3]}/>
               </tbody>
           </table>
-      </React.Fragment>
-    );
+      </React.Fragment>;
   }
 });
