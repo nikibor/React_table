1b431fc91f40cdb549a2f1edc36a416fedd7c421
nikibor <nborgolov96@gmail.com>
2018-04-03 11:21:33 +0400

    Basic component test

---
+++ app/assets/javascripts/components/users.js.jsx
@@ -0,0 +1,14 @@
+var Users = createReactClass({
+
+  render: function() {
+    return (
+      <React.Fragment>
+          <div className={"jumbotron"}>
+              <h1 className={'display-4'}>
+                  Hello World
+              </h1>
+          </div>
+      </React.Fragment>
+    );
+  }
+});
